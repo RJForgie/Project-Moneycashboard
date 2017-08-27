dc74c5584b381c5bffbfbc779af0a9cbf67c1368
Ryan Forgie <ryan.j.forgie@gmail.com>
2017-08-27 11:40:57 +0100

    Update budget working

---
+++ controllers/accountsettings_controller.rb
@@ -0,0 +1,21 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require('pry-byebug')
+require_relative('../models/tag.rb')
+require_relative('../models/transaction.rb')
+require_relative('../models/merchant.rb')
+require_relative('../models/accountsettings.rb')
+
+#INDEX route
+
+get '/accountsettings/:id/edit' do
+  @accountsettings = AccountSettings.find(params[:id])
+  erb(:"accountsettings/edit")
+end
+
+#UPDATE route
+post '/accountsettings/:id' do
+  accountsetting = AccountSettings.new(params)
+  accountsetting.update()
+  redirect to :"/accountsettings/#{params[:id]}/edit"
+end
