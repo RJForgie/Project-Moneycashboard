f2785d17535937a5548bda28decb49c61399308f
Ryan Forgie <ryan.j.forgie@gmail.com>
2017-10-02 16:49:28 +0100

    removed localhost from sqlrunner

--- db/sqlrunner.rb
+++ db/sqlrunner.rb
@@ -3,7 +3,7 @@ class SqlRunner
 
   def self.run(sql, values)
     begin
-      db = PG.connect({ dbname: 'moneycashboard', host: 'localhost'})
+      db = PG.connect({ dbname: 'moneycashboard'})
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
