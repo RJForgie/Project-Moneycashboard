b12fd769c17bc2321c502cdab80b57b7679aaf33
Ryan Forgie <ryan.j.forgie@gmail.com>
2017-10-02 18:10:08 +0100

    config sql runner

--- db/sqlrunner.rb
+++ db/sqlrunner.rb
@@ -3,7 +3,8 @@ class SqlRunner
 
   def self.run(sql, values)
     begin
-      db = PG.connect({ dbname: 'moneycashboard' host: ENV['DATABASE_URL']})
+      db = PG.connect({ dbname: 'd1h3e89kv99r98', host: 'ec2-54-225-237-64.compute-1.amazonaws.com',
+        port: 5432, user: 'yfwhoihlaasmdf', password: '846dd83f243cc1f79a6382449a078ea25ceaab36f1ccc5f1545acf7a79791f3a'})
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
